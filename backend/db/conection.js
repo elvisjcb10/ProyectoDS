d2945a9c712dfc89e810f114d893455f99261990
elvisjcb <elvisjcb10@gmail.com>
2025-07-21 03:14:36 -0500

    Backend listo conectado a Railway 2

--- backend/db/conection.js
+++ backend/db/conection.js
@@ -2,14 +2,11 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: 'mainline.proxy.rlwy.net',
-  user: 'root',
-  password: 'yvzEsOPjPWsQvSavuOTtQIkPTMGhIESS',
-  database: 'railway',
-  port: 19156,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
+   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 /* Intento de conexión para comprobar que el pool está listo */
